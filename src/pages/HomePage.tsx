01a0e3a9fed09d9cafe8f75d493cea4e35b2b24d
Mikhail <mike10581@gmail.com>
2022-10-21 15:49:37 -0400

    Layout, Header, Footer, Main page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+export function HomePage() {
+    return (
+        <div className="home_page">
+
+            <section className="section">
+                <div className="container">
+
+                    <div className="section__header">
+                        <h2 className="section__title">Lorem Ipsum</h2>
+                        <h3 className="section__subtitle">"Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                            amet, consectetur, adipisci velit..."</h3>
+                        <div className="section__text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat odio vel
+                                venenatis commodo.
+                                Aliquam vitae nisl sit amet augue pulvinar dictum. Maecenas non dui sem. Ut gravida
+                                dignissim erat eu
+                                semper. Cras quis pharetra nunc. Maecenas tristique porta tortor, at bibendum elit
+                                tempus in. Morbi
+                                iaculis non odio ut interdum. Vestibulum id enim tortor. Etiam leo nisi, interdum et
+                                elit nec, aliquet
+                                sagittis eros. Aenean et suscipit quam.
+                            </p>
+                            <p>
+                                Sed non fringilla tellus, bibendum lacinia tellus. Ut efficitur maximus enim, ut egestas
+                                tellus eleifend
+                                at. Proin volutpat tempor lorem a mollis. Aenean hendrerit, elit in rhoncus congue, nibh
+                                ex condimentum
+                                tellus, at suscipit justo ex in nulla. Nam tempor ligula eget fermentum facilisis.
+                                Quisque viverra justo
+                                quis semper volutpat. Aliquam facilisis facilisis nunc sed finibus. Aenean rhoncus
+                                tellus magna. Aenean
+                                fringilla ex ut velit varius egestas. Fusce pretium eros ut urna viverra, ut hendrerit
+                                enim vulputate.
+                                Etiam non justo eu magna luctus porta ac sit amet tortor.
+                            </p>
+                            <p>
+                                Vivamus nibh ligula, eleifend in efficitur non, facilisis sit amet diam. Curabitur
+                                varius est ac tortor
+                                vestibulum, ut lacinia lectus venenatis. Pellentesque luctus sapien vel neque vehicula,
+                                id ultrices
+                                purus consequat. Nulla posuere accumsan tortor, non sodales arcu finibus non. Curabitur
+                                sollicitudin
+                                egestas maximus. Mauris cursus justo pellentesque velit vehicula, sed tincidunt nibh
+                                fermentum. Donec
+                                sodales magna sit amet sapien feugiat lobortis. Etiam efficitur consectetur egestas.
+                            </p>
+                            <p>
+                                Vivamus ut tincidunt est. Vivamus sit amet rhoncus dolor. Fusce non tincidunt sapien.
+                                Fusce posuere
+                                mollis tristique. Aliquam in justo et nunc pulvinar placerat. Integer vel risus porta,
+                                finibus tortor
+                                id, dapibus turpis. Aliquam convallis nibh id felis dapibus fermentum. Maecenas et
+                                luctus quam, nec
+                                aliquet risus. Nullam iaculis sem ligula, id porta tortor faucibus non. Aliquam molestie
+                                eget neque id
+                                scelerisque. Ut luctus, sapien eu finibus vehicula, libero dui feugiat justo, efficitur
+                                egestas enim
+                                dolor a lectus. Donec non consequat elit. Nam ac mauris eu nulla luctus cursus. Ut
+                                mollis feugiat leo.
+                                Maecenas leo arcu, varius at magna ac, lobortis consequat nulla. Sed sit amet luctus
+                                odio, in bibendum
+                                lorem.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
