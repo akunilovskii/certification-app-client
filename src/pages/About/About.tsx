455524d07c68e7d62f41e58d389a7a220fe09aa4
Mikhail <mike10581@gmail.com>
2022-10-24 11:19:05 -0400

    Add the feature "theme"

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,20 +1,22 @@
-import React, {ReactElement, FC} from 'react';
+import React, {FC, ReactElement} from 'react';
 import {Box, Typography} from '@mui/material';
 
 const About: FC<any> = (): ReactElement => {
-  return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        backgroundColor: 'whitesmoke',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Typography variant='h3'>About</Typography>
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                backgroundColor: 'whitesmoke',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: 'auto',
+            }}
+        >
+            <Typography variant='h3' color='secondary.dark'>About</Typography>
+        </Box>
+    );
 };
 
 export default About;
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,20 +1,22 @@
-import React, {ReactElement, FC} from 'react';
+import React, {FC, ReactElement} from 'react';
 import {Box, Typography} from '@mui/material';
 
 const Products: FC<any> = (): ReactElement => {
-  return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        backgroundColor: 'whitesmoke',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Typography variant='h3'>Products</Typography>
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                backgroundColor: 'whitesmoke',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: 'auto',
+            }}
+        >
+            <Typography variant='h3' color='secondary.dark'>Products</Typography>
+        </Box>
+    );
 };
 
 export default Products;
