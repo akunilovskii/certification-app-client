76b4ac57907e445ac24bc7f7fbaf8deb4a21d806
Aleksandr Kunilovskii <{ID}+{username}@users.noreply.github.com>
2022-11-14 11:05:03 -0500

    Some changes and SideImage component

---
+++ src/components/sideImage/sideImage.tsx
@@ -0,0 +1,12 @@
+import {Paper} from '@mui/material';
+import React from 'react';
+
+function SideImage() {
+  return (
+    <Paper>
+      <div>SideImage</div>
+    </Paper>
+  );
+}
+
+export default SideImage;
--- src/pages/LoginSignUp/LoginSignUp.tsx
+++ src/pages/LoginSignUp/LoginSignUp.tsx
@@ -1,19 +1,34 @@
-import {Button, Grid} from '@mui/material';
+import {Box, Button, Grid, Paper} from '@mui/material';
 import {FC, ReactElement, useState} from 'react';
+import SideImage from '../../components/sideImage/sideImage';
 
 export const LoginSignUp: FC<any> = (): ReactElement => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <>
+    <Box
+      sx={{
+        width: '70vw',
+        minHeight: '300px',
+      }}
+    >
       <Grid container spacing={4} columns={32}>
-        <Grid xs={16}>
-          <Button variant='contained' >Login</Button>
+        <Grid item xs={16}>
+          <SideImage />
         </Grid>
-        <Grid xs={16}>
-          <Button variant='outlined' >SignUp </Button>
-        </Grid>
+        <Paper>
+          <Grid item xs={16}>
+            <Grid container>
+              <Grid item xs={8}>
+                <Button variant='contained'>Login</Button>
+              </Grid>
+              <Grid item xs={8}>
+                <Button variant='contained'>SignUp</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
       </Grid>
-    </>
+    </Box>
   );
 };
