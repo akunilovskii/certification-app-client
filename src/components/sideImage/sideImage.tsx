eea82fa76712d7dd923fe689f5170712276f9464
Aleksandr Kunilovskii <{ID}+{username}@users.noreply.github.com>
2022-11-15 09:34:39 -0500

    Make login page layout responsive. Add picture to the left

--- src/components/sideImage/sideImage.tsx
+++ src/components/sideImage/sideImage.tsx
@@ -1,11 +1,15 @@
-import {Paper} from '@mui/material';
+import {CardMedia} from '@mui/material';
 import React from 'react';
 
 function SideImage() {
   return (
-    <Paper>
-      <div>SideImage</div>
-    </Paper>
+    <CardMedia
+      component='img'
+      height='100%'
+      alt='table with a laptop'
+      image={require('../../assets/study-exam-table.jpg')}
+      sx={{objectFit: 'cover', objectPosition: 'left top'}}
+    />
   );
 }
 
--- src/pages/LoginSignUp/LoginSignUp.tsx
+++ src/pages/LoginSignUp/LoginSignUp.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Grid, Paper} from '@mui/material';
+import {Button, Container, CssBaseline, FormGroup, Grid} from '@mui/material';
 import {FC, ReactElement, useState} from 'react';
 import SideImage from '../../components/sideImage/sideImage';
 
@@ -6,29 +6,35 @@ export const LoginSignUp: FC<any> = (): ReactElement => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <Box
+    // <Container maxWidth={false}>
+    <Grid
+      container
       sx={{
-        width: '70vw',
-        minHeight: '300px',
+        width: '90%',
       }}
     >
-      <Grid container spacing={4} columns={32}>
-        <Grid item xs={16}>
-          <SideImage />
-        </Grid>
-        <Paper>
-          <Grid item xs={16}>
-            <Grid container>
-              <Grid item xs={8}>
-                <Button variant='contained'>Login</Button>
-              </Grid>
-              <Grid item xs={8}>
-                <Button variant='contained'>SignUp</Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Paper>
+      <Grid
+        item
+        sm={4}
+        sx={{
+          border: '1px solid grey',
+          height: '40vh',
+          opacity: '40%',
+          display: {xs: 'none', md: 'flex'},
+        }}
+      >
+        <SideImage />
       </Grid>
-    </Box>
+      <Grid container item sm={8} md={8} sx={{border: '1px solid grey'}}>
+        <Grid item container direction='column'>
+          <Grid sx={{border: '1px solid black'}}>
+            <Button variant='contained'>Login</Button>
+            <Button variant='contained'>SignUp</Button>
+          </Grid>
+          <FormGroup>Login Form</FormGroup>
+        </Grid>
+      </Grid>
+    </Grid>
+    // </Container>
   );
 };
