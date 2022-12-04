8ec811a70f20ca4e0a8c779f375a958e8b1360e7
Mikhail <mike10581@gmail.com>
2022-12-04 09:46:56 -0500

    add creating test with empty questions[]

---
+++ src/components/QuestionsForm.tsx
@@ -0,0 +1,11 @@
+import {FC, ReactElement} from "react";
+
+const QuestionsForm: FC<any> = (): ReactElement => {
+    return (
+        <div>
+            QUESTIONS FORM
+        </div>
+    )
+}
+
+export default QuestionsForm;
--- src/pages/CreateTest/CreateTest.tsx
+++ src/pages/CreateTest/CreateTest.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement, useContext } from 'react'
+import {FC, ReactElement, useContext, useState} from 'react'
 import {
   Button,
   FormControl,
@@ -6,26 +6,40 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  TextField,
 } from '@mui/material'
-
-import DataTable from '../Tests/DataTable'
 import useFilter from '../../hook/use-filter'
 import DataContext from '../../context/data-context'
-import { tests } from '../../store/tests-store'
+import {tests} from '../../store/tests-store'
+import QuestionsForm from "../../components/QuestionsForm";
 
 const CreateTest: FC<any> = (): ReactElement => {
+  const [testID, setTestID] = useState(null);
+  console.log(testID);
   const [disciplineProps, resetDiscipline] = useFilter('')
   const [levelProps, resetLevel] = useFilter('')
   const [subjectProps, resetSubject] = useFilter('')
-  const { setItemsList } = useContext(DataContext)
+  const [titleProps, resetTitle] = useFilter('')
+  const [difficultyProps, resetDifficulty] = useFilter('')
+  const [durationProps, resetDuration] = useFilter(0)
 
-  const resetFilters = () => {
-    // send tests
-    console.log('TestList: ', testsList)
+  const {setItemsList} = useContext(DataContext)
+
+  const createTest = () => {
+    // create empty test
+    const emptyTest = {
+      //@ts-ignore
+      title: titleProps.value,
+      //@ts-ignore
+      difficulty: difficultyProps.value,
+      //@ts-ignore
+      duration: durationProps.value,
+      questions: [{question: 'Q1', answers: []},{question: 'Q2', answers: []}],
+    }
 
     const requestOptions = {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         data: {
           //@ts-ignore
@@ -34,134 +48,161 @@ const CreateTest: FC<any> = (): ReactElement => {
           subject: subjectProps.value,
           //@ts-ignore
           level: levelProps.value,
-          tests: testsList,
+          test: emptyTest,
         },
       }),
     }
     fetch('http://localhost:5000/tests/create', requestOptions)
-      .then((response) => response.json())
-      .then((data) => console.log('Create test request -> data: ', data))
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Data from createEmptyTest response: ', data);
+          setTestID(data._id);
+        })
+
   }
 
-  const testsList = setItemsList(
-    {
-      // @ts-ignore
-      discipline: disciplineProps.value,
-      // @ts-ignore
-      level: levelProps.value,
-      // @ts-ignore
-      subject: subjectProps.value,
-    },
-    'tests'
-  ).flatMap((el: any) => el.value.map((res: any) => res))
-
   return (
-    <>
-      <Grid container item md={8} xs={12} justifyContent="center">
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-          <InputLabel id="discipline-label">Discipline</InputLabel>
-          <Select
-            labelId="discipline-label"
-            id="discipline"
-            {...disciplineProps}
-            label="Discipline"
-          >
-            <MenuItem value="">
-              <em>All disciplines</em>
-            </MenuItem>
-            {setItemsList(
-              {
-                // @ts-ignore
-                level: levelProps.value,
-                // @ts-ignore
-                subject: subjectProps.value,
-              },
-              'discipline'
-            ).map((el: any) => (
-              <MenuItem key={el.id} value={el.value}>
-                {el.value}
+      <>
+        <Grid container item md={8} xs={12} justifyContent="center">
+          <FormControl variant="standard" sx={{m: 1, minWidth: 120}}>
+            <InputLabel id="discipline-label">Discipline</InputLabel>
+            <Select
+                labelId="discipline-label"
+                id="discipline"
+                {...disciplineProps}
+                label="Discipline"
+            >
+              <MenuItem value="">
+                <em>All disciplines</em>
               </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-          <InputLabel id="level-label">Level</InputLabel>
-          <Select
-            labelId="level-label"
-            id="level"
-            {...levelProps}
-            label="Level"
-          >
-            <MenuItem value="">
-              <em>All levels</em>
-            </MenuItem>
-            {setItemsList(
-              {
-                // @ts-ignore
-                discipline: disciplineProps.value,
-                // @ts-ignore
-                subject: subjectProps.value,
-              },
-              'level'
-            ).map((el: any) => (
-              <MenuItem key={el.id} value={el.value}>
-                {el.value}
+              {setItemsList(
+                  {
+                    // @ts-ignore
+                    level: levelProps.value,
+                    // @ts-ignore
+                    subject: subjectProps.value,
+                  },
+                  'discipline'
+              ).map((el: any) => (
+                  <MenuItem key={el.id} value={el.value}>
+                    {el.value}
+                  </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl variant="standard" sx={{m: 1, minWidth: 120}}>
+            <InputLabel id="level-label">Level</InputLabel>
+            <Select
+                labelId="level-label"
+                id="level"
+                {...levelProps}
+                label="Level"
+            >
+              <MenuItem value="">
+                <em>All levels</em>
               </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-          <InputLabel id="subject-label">Subject</InputLabel>
-          <Select
-            labelId="subject-label"
-            id="subject"
-            {...subjectProps}
-            label="Subject"
-          >
-            <MenuItem value="">
-              <em>All subjects</em>
-            </MenuItem>
-            {setItemsList(
-              {
-                // @ts-ignore
-                discipline: disciplineProps.value,
-                // @ts-ignore
-                level: levelProps.value,
-              },
-              'subject'
-            ).map((el: any) => (
-              <MenuItem key={el.id} value={el.value}>
-                {el.value}
+              {setItemsList(
+                  {
+                    // @ts-ignore
+                    discipline: disciplineProps.value,
+                    // @ts-ignore
+                    subject: subjectProps.value,
+                  },
+                  'level'
+              ).map((el: any) => (
+                  <MenuItem key={el.id} value={el.value}>
+                    {el.value}
+                  </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl variant="standard" sx={{m: 1, minWidth: 120}}>
+            <InputLabel id="subject-label">Subject</InputLabel>
+            <Select
+                labelId="subject-label"
+                id="subject"
+                {...subjectProps}
+                label="Subject"
+            >
+              <MenuItem value="">
+                <em>All subjects</em>
               </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+              {setItemsList(
+                  {
+                    // @ts-ignore
+                    discipline: disciplineProps.value,
+                    // @ts-ignore
+                    level: levelProps.value,
+                  },
+                  'subject'
+              ).map((el: any) => (
+                  <MenuItem key={el.id} value={el.value}>
+                    {el.value}
+                  </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl variant="standard" sx={{m: 1, minWidth: 120}}>
+            <TextField
+                label="Test title"
+                type="text"
+                // fullWidth
+                size="small"
+                {...titleProps}
+            />
+          </FormControl>
+          <FormControl variant="standard" sx={{m: 1, minWidth: 120}}>
+            <Select
+                labelId="subject-difficulty"
+                id="difficulty"
+                {...difficultyProps}
+                label="Difficulty"
+            >
+              <MenuItem value="easy">Easy</MenuItem>
+              <MenuItem value="standard">Standard</MenuItem>
+              <MenuItem value="hard">Hard</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl variant="standard" sx={{m: 1, minWidth: 120}}>
+            <TextField
+                label="Duration"
+                type="text"
+                // fullWidth
+                size="small"
+                {...durationProps}
+            />
+          </FormControl>
 
+          {/*<DataTable testsList={testsList} />*/}
+        </Grid>
         <FormControl
-          variant="standard"
-          sx={{ m: 1, minWidth: 120, justifyContent: 'flex-end' }}
+            variant="standard"
+            sx={{m: 1, minWidth: 120, justifyContent: 'flex-end'}}
         >
           <Button
-            color="primary"
-            variant="outlined"
-            size="small"
-            disabled={
-              // @ts-ignore
-              disciplineProps.value === '' ||
-              // @ts-ignore
-              levelProps.value === '' ||
-              // @ts-ignore
-              subjectProps.value === ''
-            }
-            onClick={() => resetFilters()}
+              color="primary"
+              variant="outlined"
+              size="small"
+              disabled={
+                // @ts-ignore
+                  disciplineProps.value === '' ||
+                  // @ts-ignore
+                  levelProps.value === '' ||
+                  // @ts-ignore
+                  subjectProps.value === '' ||
+                  // @ts-ignore
+                  titleProps.value === '' ||
+                  // @ts-ignore
+                  difficultyProps.value === ''
+              }
+              onClick={() => createTest()}
           >
-            Send tests into mongo
+            Create test
           </Button>
         </FormControl>
 
-        <DataTable testsList={testsList} />
-      </Grid>
-    </>
+        {testID && <QuestionsForm />}
+      </>
   )
 }
 
