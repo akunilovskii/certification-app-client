0a2ad177a3a3d93e1aa7718136150eed4bbf589f
Mikhail <mike10581@gmail.com>
2023-01-29 19:38:51 -0500

    add content for about page add readme.md

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,44 @@
-import {FC, ReactElement} from 'react';
-import {Typography} from '@mui/material';
+import React, {FC, ReactElement} from 'react'
+import {Box, Typography} from '@mui/material'
 
 const About: FC<any> = (): ReactElement => {
-  return (
-    <>
-      <Typography variant='h3' color='text.primary'>
-        About
-      </Typography>
-    </>
-  );
-};
+    return (
+        <Box
+            sx={{
+                maxWidth: '800px',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '30px',
+            }}
+        >
+            <Typography
+                variant="h4"
+                color="text.primary"
+                sx={{textAlign: 'center'}}
+            >
+                Welcome to our Full Stack project!
+            </Typography>
+            <Typography variant="body1" color="text.primary">
+                &emsp;&emsp;We are a team of two aspiring developers who embarked on
+                this project with the goal of learning new technologies and skills. Over
+                the course of two months, we dove into the world of React, JWT-based
+                authentication and authorization, Node.JS, TypeScript, MongoDB, CI/CD,
+                and implementing modular architecture.
+            </Typography>
+            <Typography variant="body1" color="text.primary">
+                &emsp;&emsp;Throughout the development process, we also focused on
+                utilizing best practices for Git workflow and collaboration.
+            </Typography>
+            <Typography variant="body1" color="text.primary">
+                &emsp;&emsp;We are proud of what we have accomplished and are excited to
+                share our project with the world. We hope you enjoy using it as much as
+                we enjoyed building it.
+            </Typography>
+            <Typography variant="body1" color="text.primary">
+                &emsp;&emsp;Thank you for visiting our about page.
+            </Typography>
+        </Box>
+    )
+}
 
 export default About;
